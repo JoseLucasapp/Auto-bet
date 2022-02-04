e7107430828a7c63dd5085e61a4d62e3faa6c86d
José Lucas <josluc2016@gmail.com>
2022-02-03 21:45:07 -0300

    feat: some logs added

--- function.js
+++ function.js
@@ -10,6 +10,8 @@ module.exports = async (data) => {
     const date = data.date
     const totalField = totalFieldFunc(total)
 
+    console.log(data, totalField)
+
     Array.prototype.duplicates = function () {
         return this.filter(
             function (x, y, k) {
@@ -24,7 +26,10 @@ module.exports = async (data) => {
         return `Os seguintes números estão repetidos: ${duplicados.map((n) => n)}`
     }
 
+    console.log('ok')
+
     const browser = await puppeteer.launch({ headless: true });
+    console.log('ok 1')
     const page = await browser.newPage();
     await page.goto('http://www.easybets.com.br/UniBetsPatos/seninhas.xhtml', { waitUntil: 'networkidle2' });
 
@@ -32,6 +37,7 @@ module.exports = async (data) => {
     const radioId = `label[for="j_idt81:campoQtd:${totalField}"]`
     await page.click(radioId, { clickCount: 1 })
     await numberClicks(total, page, numbers)
+    console.log('ok2')
 
     await page.waitForSelector('input[name="j_idt81:campoJogador"]');
     await page.type('input[name="j_idt81:campoJogador"]', player)
@@ -42,15 +48,19 @@ module.exports = async (data) => {
 
     await page.type('input[name="j_idt81:campoData_input"]', date)
 
+    console.log('ok3')
     await page.waitForTimeout(10000)
     await page.click('span[class="ui-button-icon-left ui-icon ui-c fa fa-check"]', { clickCount: 1 })
     await page.waitForTimeout(1000)
     await page.waitForSelector('div[id="j_idt115"]', { visible: true })
     await page.waitForTimeout(1000)
+    console.log('ok4')
     const element = await page.waitForSelector('#j_idt115 > div > div > form > center > div > div > div > div > h3', { visible: true }); // select the element
     await page.waitForTimeout(1000)
     const code = await element.evaluate(el => el.textContent)
 
+    console.log('ok5')
     await browser.close();
+    console.log('ok6')
     return code
 };
